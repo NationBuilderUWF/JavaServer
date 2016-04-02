63938c8e6c26cdf807b37046df760bb3823b6d24
jas0nea <jason.monforton@gmail.com>
2016-04-01 23:49:51 -0500

    Beginning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
+import java.sql.*;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
+        try
+        {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionUrl = "jdbc:sqlserver://localhost:797";
+            Connection con = null;
+            con = DriverManager.getConnection(connectionUrl,"the_user","the_password");
+            System.out.println("Connected to Server");
+
+            
+
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.print("Didn't connect to server");
+        }
+        catch (SQLException e)
+        {
+            System.out.print("SQL problem");
+        }
+
+
 	// write your code here
     }
 }
