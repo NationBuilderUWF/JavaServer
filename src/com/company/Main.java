dcf505f7700c78356c947f6f79243ae15a4d460d
Ben <bjc9001@gmail.com>
2016-04-02 03:25:35 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,12 @@
 package com.company;
+
 import WebUtilities.LoginRes;
 
 import java.io.*;
 import java.net.*;
 
 
-
 public class Main {
-
-
 
 
     public static void main(String[] args) {
@@ -20,39 +18,53 @@ public class Main {
         String message;
         LoginRes response = new LoginRes();
 
-        try
-        {
+        try {
             providerSocket = new ServerSocket(3000);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(e);
         }
 
-        try
-        {
-            clientSocket = providerSocket.accept();
-            is = new ObjectInputStream(clientSocket.getInputStream());
-            os = new ObjectOutputStream(clientSocket.getOutputStream());
-            while(true)
-            {
-
-                message = is.readObject().toString();
-                System.out.println(message);
-                response.admin = true;
-                response.success = true;
-                os.writeObject(response);
-            }
+        try {
+            while(true) new Main().takeNewConnection( providerSocket.accept());
+//            is = new ObjectInputStream(clientSocket.getInputStream());
+//            os = new ObjectOutputStream(clientSocket.getOutputStream());
+//            while (true) {
+//
+//                message = is.readObject().toString();
+//                System.out.println(message);
+//                response.admin = true;
+//                response.success = true;
+//                os.writeObject(response);
+//            }
 
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(e);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
 
 
         // write your code here
     }
+
+    public boolean takeNewConnection(Socket socket) {
+        try {
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
+            while (true) {
+
+                LoginRes response = new LoginRes();
+
+
+                System.out.println("  ");
+                response.admin = true;
+                response.success = true;
+                os.writeObject(response);
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
